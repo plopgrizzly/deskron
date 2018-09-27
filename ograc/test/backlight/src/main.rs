3ffbc4959deb6f631abd436998e982e2b53f8629
Jeron A. Lau <jeron.lau@plopgrizzly.com>
2018-09-26 23:50:40 -0500

    Added increment and decrement functionality to backlight command.

--- ograc/test/backlight/src/main.rs
+++ ograc/test/backlight/src/main.rs
@@ -1,6 +1,7 @@
 // use std::process::Command;
 use std::{ env, fs, process, io };
 use std::io::{ Write };
+use std::path::PathBuf;
 
 /*fn run(cmd: &str, args: &[&str]) {
 	let mut builder = Command::new(cmd);
@@ -11,11 +12,17 @@ use std::io::{ Write };
 		.wait().expect("command wasn't running");
 }*/
 
+fn read_light(light_max: f32, light: &PathBuf) -> std::io::Result<f32> {
+    // Read Light
+    Ok(fs::read_to_string(light)?.lines().next().unwrap()
+        .parse::<i32>().unwrap() as f32 * 100.0 / light_max)
+}
+
 fn main() -> std::io::Result<()> {
 	let args: Vec<String> = env::args().collect();
 
 	if args.len() != 2 {
-		println!("Usage:\n\tbacklight 50\n\nSet to 50%");
+		println!("Usage:\n\tbacklight 50\t\tSet to 50%\n\tbacklight +\t\tIncrease Brightness\n\tbacklight -\t\tDecrease Brightess");
 		process::exit(0);
 	}
 
@@ -35,8 +42,21 @@ fn main() -> std::io::Result<()> {
 	let lightm = fs::read_to_string(&maxlt)?.lines().next().unwrap()
 		.parse::<i32>().unwrap() as f32;
 
+    // Calculate percent
+    let mut percent = match args[1].as_str() {
+        "inc" => read_light(lightm, &light)? + 5.0,
+        "dec" => read_light(lightm, &light)? - 5.0,
+        _ => args[1].parse::<i32>().unwrap() as f32,
+    };
+
+    if percent <= 0.0 {
+        percent = 0.0;
+    } else if percent >= 100.0 {
+        percent = 100.0;
+    }
+
 	// Write Light
-	let percent = args[1].parse::<i32>().unwrap() as f32;
+//	let percent = args[1].parse::<i32>().unwrap() as f32;
 	fs::File::create(&light)?.write_fmt(format_args!("{}",
 		(percent * lightm / 100.0).round() as i32))?;
 
